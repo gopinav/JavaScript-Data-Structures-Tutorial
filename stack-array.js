dd7c6b47f4da446708d26a27ebf5f5b9e3135d91
Vishwas <vishwas.g5@gmail.com>
2022-05-15 15:37:25 +0530

    Implement stack data structure using array

---
+++ stack-array.js
@@ -0,0 +1,40 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+stack.push(20);
+stack.push(10);
+stack.push(30);
+console.log(stack.size());
+stack.print();
+console.log(stack.pop());
+console.log(stack.peek());
+stack.print();
