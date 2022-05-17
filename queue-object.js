4aafa664bfcc4e8666ae8dffae74220024680ad6
Vishwas <vishwas.g5@gmail.com>
2022-05-17 08:05:56 +0530

    Optimised queue implementation

---
+++ queue-object.js
@@ -0,0 +1,47 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.size());
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
+console.log(queue.isEmpty());
+queue.print();
